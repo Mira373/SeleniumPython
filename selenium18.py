43690cb84611128b7e308d14ccbfde8ad244c672
Miranda Begashvili <begashvilimira@gmail.com>
2022-10-09 16:05:56 +0400

    selenium second round

---
+++ selenium18.py
@@ -0,0 +1,34 @@
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.select import Select
+from Screenshot import Screenshot
+from selenium.webdriver.common.alert import Alert
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+
+#Perform Right Click & Double Click
+
+
+class DemoMouseActions:
+    def mouse_actions(self):
+        driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+        driver.get('https://github.com/')
+        driver.maximize_window()
+        nav_button = driver.find_element(By.XPATH,"/html/body/div[1]/header/div/div[2]/div/nav/ul/li[3]/button")
+        actions = ActionChains(driver)
+        actions.context_click(nav_button).perform() #right click --> mouse menu
+        actions.double_click(nav_button).perform()  #Double-clicks an element
+        time.sleep(10)
+
+"""
+context_click(on_element=None)
+Performs a context-click (right click) on an element.
+"""
+
+demo_mouse_actions = DemoMouseActions()
+demo_mouse_actions.mouse_actions()
